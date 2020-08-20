c131910a4cb1d588e057bc7d135b24fc2392965e
Jim O'Regan <jaoregan@tcd.ie>
2020-08-20 11:34:17 +0100

    I'm actually using Colab, but why not

---
+++ run-tokeniser.py
@@ -0,0 +1,22 @@
+import sys
+from transformers import *
+model = BertForMaskedLM.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
+tokenizer = BertTokenizer.from_pretrained("dkleczek/bert-base-polish-uncased-v1")
+
+if len(sys.argv) != 3:
+    print("Usage: run-tokeniser.py [lattice] [output]")
+    sys.exit()
+
+lat = open(file=sys.argv[1], mode="r", encoding="utf-8")
+out = open(file=sys.argv[2], mode="a", encoding="utf-8")
+for line in lat:
+    if not ' ' in line.strip():
+        continue
+    else:
+        parts = line.strip().split(" ")
+        if len(parts) != 4:
+            continue
+        else:
+            word = parts[2]
+            toks = tokenizer.tokenize(word)
+            out.print("%s\t%s" % (word, " ".join(toks)))
