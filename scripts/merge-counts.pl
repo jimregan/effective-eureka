ca099705923005d32415fc3b367a62dee854edc3
Jim O'Regan <jaoregan@tcd.ie>
2020-09-04 21:24:25 +0100

    merge counts (and put counts at the end, for opengrm ngram stuff)

---
+++ scripts/merge-counts.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+use warnings;
+use strict;
+use utf8;
+
+my %ngrams = ();
+
+binmode(STDIN, ":utf8");
+binmode(STDOUT, ":utf8");
+
+while(<>) {
+	chomp;
+	my @line = split/ /;
+	my $count = shift @line;
+	my $ngram = join(' ', @line);
+	if(!exists $ngrams{$ngram}) {
+		$ngrams{$ngram} = $count;
+	} else {
+		$ngrams{$ngram} += $count;
+	}
+}
+for my $ngram (keys %ngrams) {
+	print "$ngram $ngrams{$ngram}\n";
+}
